495c171451b317c702e9b00cb5589a199f14d53e
vpfof <parkkinenv@gmail.com>
2022-01-17 11:10:49 +0100

    graph sketch

--- scraps.R
+++ scraps.R
@@ -9,11 +9,38 @@ rhs <- lapply(asfs[[1]], rhs)
 # literals <- unlist(literals, recursive = FALSE)
 disjuncts <- lapply(lhs, function(x) strsplit(x, "\\+"))
 #disjuncts <- lapply(lhs, function(x) strsplit(gsub("\\*", "", x), "\\+"))
-disjuncts <- unlist(disjuncts, recursive = FALSE)
-rhsreps <- mapply(function(x, y) rep(x, length(y)), rhs, disjuncts, SIMPLIFY = FALSE)
-edgelist <- data.frame(out=rev(unlist(rhsreps)), disj=rev(unlist(disjuncts)))
-edgelist <- graph.edgelist(as.matrix(edgelist))
+#disjuncts <- unlist(disjuncts, recursive = FALSE)
+disjuncts <- unlist(target_lhss_disjuncts, recursive = FALSE)
+rhs <- target_rhss
+
+
+
+rhsreps <- vector("list", length(target_rhss))
+for (i in seq_along(target_rhss)){
+  rhsreps[[i]] <- mapply(function(x, y) rep(x, length(y)), target_rhss[i], target_lhss_facs[[i]], SIMPLIFY = FALSE)  
+}
+
+
+
+edgelist <- data.frame(disj=unlist(target_lhss_facs), out=unlist(rhsreps))
+graph <- graph.edgelist(as.matrix(edgelist))
+
+cand_facs <- unlist(strsplit(cand_disjuncts, "\\*"))
+cand_fac_paths <- vector("list", length(cand_facs))
+for(fac in seq_along(cand_facs)){
+  temppaths <- all_simple_paths(graph, from = cand_facs[fac], to = outcome)
+  cand_fac_paths[[fac]] <- lapply(temppaths, function(x) as.character(names(x)))
+  if(length(cand_fac_paths[[fac]]) == 0L) {cand_fac_paths[[fac]] <- NA}
+  
+}
+names(cand_fac_paths) <- cand_facs
+
+cand_fac_paths
+
+lapply(cand_fac_paths, function(x) lapply(x, function(y) which(target_rhss %in% y)))
+
 adj_matrix <- as.matrix(get.adjacency(edgelist))
+all_simple_paths(adj_matrix)
 adj_matrix[adj_matrix == 0] <- NA
 
 paths <- floyd(adj_matrix)
@@ -40,7 +67,7 @@ outs <- target_rhss
 pathfinder(diss, outs, c())
 
 pathfinder <- function(diss, outs, path){
-  path <- c(path, diss[[1]])
+  path <- c(path, diss[[1]], outs[1])
   #lapply(diss[-1], function(x) pmatch(x, outs[-1]))
   idx <- lapply(outs[1], function(x) which(!is.na(pmatch(x, outs))))
   #idx <- lapply(idx, function(x) x[!is.na(x)])
